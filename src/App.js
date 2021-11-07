cb7d36a6d95fabbab42820625ceab9a2fc82bbb7
Enzo Ventura <enzo.ventura@epitech.eu>
2021-11-07 18:09:02 +0100

    Added custom fonts, first page

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import './App.css';
 
 function App() {
-  return (
-    <div>
-    </div>
-  );
+    return (
+        <div className="w-full">
+            <div className="w-full h-screen flex justify-center items-center">
+                <h1 className="text-7xl font-Exo2-SemiBold">awaiting developer...</h1>
+            </div>
+        </div>
+    );
 }
 
 export default App;
