5781650d5bec62092f1cb18b51ac3504e9964684
Enzo Ventura <enzo.ventura@epitech.eu>
2021-11-07 20:55:13 +0100

    Added loading spinner

--- src/App.js
+++ src/App.js
@@ -3,8 +3,15 @@ import './App.css';
 function App() {
     return (
         <div className="w-full">
-            <div className="w-full h-screen flex justify-center items-center">
+            <div className="w-full h-screen flex flex-col justify-center items-center">
                 <h1 className="text-7xl font-Exo2-SemiBold">awaiting developer...</h1>
+                <div className="spinner">
+                    <div className="rect1 mx-1"/>
+                    <div className="rect2"/>
+                    <div className="rect3 mx-1"/>
+                    <div className="rect4"/>
+                    <div className="rect5 mx-1"/>
+                </div>
             </div>
         </div>
     );
