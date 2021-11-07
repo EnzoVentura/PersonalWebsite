d562005e02d9d7b2769f54fcf2e6a453c3091c79
Enzo Ventura <enzo.ventura@epitech.eu>
2021-11-07 21:42:21 +0100

    multilingual website

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
 import './App.css';
+import {Suspense} from "react";
+import {useTranslation} from "react-i18next";
+import i18n from "i18next";
 
 function App() {
+    const {t} = useTranslation();
+    const changeLanguage = (lng) => {
+        i18n.changeLanguage(lng);
+    }
+
     return (
         <div className="w-full">
             <div className="w-full h-screen flex flex-col justify-center items-center">
-                <h1 className="text-7xl font-Exo2-SemiBold">awaiting developer...</h1>
+                <h1 className="text-7xl font-Exo2-SemiBold">{t("Wait.awaiting")}</h1>
                 <div className="spinner">
                     <div className="rect1 mx-1"/>
                     <div className="rect2"/>
@@ -16,5 +24,10 @@ function App() {
         </div>
     );
 }
-
-export default App;
+export default function WrappedApp() {
+    return (
+        <Suspense fallback="Loading">
+                <App/>
+        </Suspense>
+    );
+}
